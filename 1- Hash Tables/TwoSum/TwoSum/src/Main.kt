a3adaa68359afd6e3b36158a00df0c8542998287
MedCh25 <medresse25@gmail.com>
2025-08-02 11:00:41 -0400

    Commit : find all the numbers x in nums that satisfy the following: x + 1 is not in nums, and x - 1 is not in nums

--- 1- Hash Tables/TwoSum/TwoSum/src/Main.kt
+++ 1- Hash Tables/TwoSum/TwoSum/src/Main.kt
@@ -1,6 +1,9 @@
 fun main() {
     println("Hello getIndices")
     println(getIndices(arrayOf(5,2,7,10,3,9), 8))
+
+    println("Hello finAllNumbers")
+    println(finAllNumbers(arrayOf(5,4,6,3,2,9,10,8,15)))
 }
 
 /*
@@ -20,4 +23,26 @@ fun getIndices(array : Array<Int>,target : Int) : String{
     }
 
     return "No result"
+}
+
+/*
+
+Given an integer array nums, find all the numbers x in nums that satisfy the following: x + 1 is not in nums, and x - 1 is not in nums.
+
+If a valid number x appears multiple times, you only need to include it in the answer once.
+
+ */
+
+fun finAllNumbers(array : Array<Int>) : HashSet<Int> {
+
+    val answers = hashSetOf<Int>()
+    val hashSetOfArray = array.toHashSet()
+
+    for (item in hashSetOfArray){
+        if(!hashSetOfArray.contains(item + 1) && !hashSetOfArray.contains(item -1)){
+            answers.add(item)
+        }
+    }
+
+    return answers
 }
