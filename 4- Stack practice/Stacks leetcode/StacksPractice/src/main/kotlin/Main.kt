fd3cfb5698dd903b0499b06300eb3950db34f810
Mohamed C <medresse25@gmail.com>
2025-08-14 08:23:52 -0400

    Remove All Adjacent Duplicates In string using Stack

--- 4- Stack practice/Stacks leetcode/StacksPractice/src/main/kotlin/Main.kt
+++ 4- Stack practice/Stacks leetcode/StacksPractice/src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 package org.example
 
 fun main() {
-    println("Valid Parentheses example!")
-    println(isInputValid(")"))
+    //println("Valid Parentheses example!")
+    //println(isInputValid(")"))
+
+    println("Remove All Adjacent Duplicates In String")
+    println(removeDuplicates("azxxzy"))
 }
